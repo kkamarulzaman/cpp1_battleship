a37865afbcf412a40158334f25f72564c843fe7c
kkamarulzaman <103600288+kkamarulzaman@users.noreply.github.com>
2023-04-16 20:33:25 -0500

    Update game.h

--- lib/game.h
+++ lib/game.h
@@ -35,7 +35,17 @@ protected:
 class AssignmentGame : public BaseGame {
   protected:
     int solve() override {
-        //IMPLEMENT YOUR SEARCH ALGORITHM HERE
+        int SHIP_COUNT = 0;
+        for(int i = 0; i < HEIGHT - 1; i++) 
+        { 
+          for(int j = 0; j < WIDTH - 1; j++)
+          {
+            if((this->board->guess(i,j) == ResponseType::HIT)
+               { 
+                 SHIP_COUNT += 1;
+               }
+          }
+        }
         return SHIP_COUNT;
     }
 };
